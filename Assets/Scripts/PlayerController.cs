fda939a2fc40c60da4ad5e52e9cbd9a53dda9384
nafisa-x <nafisa.x.shaikh@gmail.com>
2023-10-06 10:26:05 +0100

    added Pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;  
     public float speed;
+    private int count;
+
     void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
@@ -14,4 +16,15 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime); 
         } 
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count++;
+        }
+    }
+
+    void Start() {
+        count = 0;
+    }
 }
