bec975d1651fdaf1a7ae211cb1aaa3ae617ddc36
nafisa-x <nafisa.x.shaikh@gmail.com>
2023-10-06 11:35:08 +0100

    Added Score and Win UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;  
     public float speed;
     private int count;
+    private int numPickups = 5;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
     void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
-    }  
+    } 
+
+    void Start() {
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
     void FixedUpdate() { 
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime); 
@@ -21,10 +33,16 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "PickUp") {
             other.gameObject.SetActive(false);
             count++;
+            SetCountText();
         }
     }
 
-    void Start() {
-        count = 0;
+    private void SetCountText(){
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups){
+            winText.text = "You win!";
+        }
     }
+
+    
 }
